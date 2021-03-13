2264b75f2f0d582e224548f009e6b3d1230ab5e0
LexieAlreadyTaken <Lexie_Li@outlook.com>
2021-03-13 23:02:29 +0800

    不太成功的mongodb

--- src/components/Footprint.vue
+++ src/components/Footprint.vue
@@ -1,6 +1,7 @@
 <template>
   <el-row>
     数据库还没有建好，不过可以加我的粉丝群表示支持？
+    <el-button @click="handleClick">一个测试按钮，可以点点看。</el-button>
   </el-row>
 </template>
 
@@ -13,10 +14,28 @@ export default {
   },
   data(){
     return{
+      MongoClient : null,
+      url: 'mongodb://localhost:27017/raku'
     }
   },
-    methods: {
+  mounted(){
+  },
+  methods: {
+    handleClick(event){
+      console.log(event);
+      this.MongoClient = require('mongodb').MongoClient;
+      this.MongoClient.connect(this.url, function(err, db){
+        if(err) throw err;
+        var dbo = db.db("raku");
+        var myobj = { name: "", time:new Date()};
+        dbo.collection("visitor").insertOne(myobj, function(err,res){
+          if(err) throw err;
+          console.log(res);
+          db.close();
+        })
+      })
     }
+  }
 }
 </script>
 
